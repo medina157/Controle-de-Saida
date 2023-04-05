7117790386f6f8ddc0062658b0f178861b21c9fb
medina157 <mdnrdrgs@gmail.com>
2023-04-05 15:26:06 -0300

    .

--- js/login.js
+++ js/login.js
@@ -1,10 +1,11 @@
-import { auth } from "../module.js"
+import { auth } from "js/firebase.js"
 
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
 
 document.getElementById("login").addEventListener("click", () => {
     let email = document.getElementById("email").value
     let senha = document.getElementById("senha").value
+    alert("[SUCESSO] E-mail autenticado com sucesso.")
 
     const autenticacao = getAuth()
     signInWithEmailAndPassword(autenticacao, email, senha)
@@ -17,3 +18,4 @@ document.getElementById("login").addEventListener("click", () => {
             alert("Erro no login.")
         });
 })
+
