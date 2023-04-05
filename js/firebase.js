877af416732d041cc21fd7cd1bdaf66c9280ceaa
medina157 <mdnrdrgs@gmail.com>
2023-04-05 13:47:22 -0300

    .

--- js/firebase.js
+++ js/firebase.js
@@ -13,65 +13,9 @@ import { initializeApp, getAuth } from "firebase/app";
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
-  function Cadastrar(){
-    var auth = getAuth(app);
-
-    firebase.auth().createUserwithEmailAndPassword(document.getElementById("Email").value,document.getElementById("password").value)
-    .then(function (user) {
-        alert("Login Efetuado com sucesso");
-        auth = user;
-
-        //para atualizar o navegador
-        document.getElementById("email").value = ''
-        document.getElementById("senha").value = ''
-    }).cath(function(error){
-        alert("falha no Login")
-    })
-  }
+  
 
 
 
 
 
-
-/*import { database, auth} from "./modules.js"
-import { ref, update } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-import {signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-
-
-  //Login do usuário com email e senha criado
-
-  login.addEventListener('click',(e) =>{
-
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        
-        const user = userCredential.user;
-
-        const dt = new Date();
-        update(ref(database, 'users/' + user.uid ),{
-          ultimo_login: dt,
-        })
-        alert('Usuário logado!');
-        
-      })
-    .catch((error) => {
-        const errorCode = error.code;
-      
-          if (errorCode == "auth/user-not-found"){
-            alert("Usuário não existente, por favor realize o cadastro.")
-          }else if(errorCode == "auth/invalid-email"){
-              alert("Email inválido")
-            }else if (errorCode == "auth/wrong-password"){
-                alert("Email ou senha incorreta!")
-              }else if(errorCode == "auth/internal-error"){
-                alert("Ops um erro ocorreu, tente novamente mais tarde")
-                }else {
-                  alert(errorCode, "Ops um erro ocorreu")}
-      });
-
-  })
-*/
