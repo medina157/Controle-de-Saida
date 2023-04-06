3da58066b7246c05ce83b9793688f55564848785
medina157 <mdnrdrgs@gmail.com>
2023-04-06 14:35:14 -0300

    .

---
+++ js/painel.js
@@ -0,0 +1,14 @@
+let dashboard = document.getElementById("dashboard")
+
+let card = document.createElement("div")
+card.setAttribute("class", "card")
+
+let h2 = document.createElement("h2")
+h2.setAttribute("class", "h2nome")
+
+let ptexto = document.createElement("p")
+ptexto.setAttribute("class", "ptexto")
+
+let phora = document.createElement("p")
+phora.setAttribute("class", "hora")
+
