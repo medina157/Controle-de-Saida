e91e44c3160e25df5c6870dc8e22ec8f96f1ab44
medina157 <mdnrdrgs@gmail.com>
2023-04-06 14:21:20 -0300

    .

--- js/CadSaida.js
+++ js/CadSaida.js
@@ -2,3 +2,17 @@ import {db} from "../js/firebase.js"
 
 import { addDoc, collection } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js"
 
+document.getElementById("registrarsaida").addEventListener("click", function(){
+    let nomefuncionario = document.getElementById("nome").value
+    let departamento = document.getElementById("departamento").value
+    let horario = document.getElementById("horario").value
+    alert(`${nomefuncionario} ${departamento} ${horario}`)
+
+    addDoc(collection(db, "funcionario"), {
+        nome: nomefuncionario, 
+        departamento: departamento,
+        hora:horario
+    });
+
+
+})
